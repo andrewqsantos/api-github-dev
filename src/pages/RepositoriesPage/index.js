79f12d244700dc584386aa97fa4c559fb57c159b
andrewqsantos <andrewqsantos@protonmail.com>
2022-01-10 10:04:17 -0300

    componente filter

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -7,12 +7,66 @@ import Repositories from './Repositories';
 
 import { Container, Sidebar, Main } from './styles';
 
+import { getLangsFrom } from '../../services/api';
+
 const RepositoriesPage = () => {
+  const user = {
+    login: 'andrewqsantos',
+    name: 'Andrew Quenehen',
+    avatar_url: 'https://avatars.githubusercontent.com/u/56467992?v=4',
+    followers: 36,
+    following: 59,
+    company: null,
+    blog: null,
+    location: 'Brazil',
+  };
+
+  const repositories = [
+    {
+      name: 'Repo 1',
+      desciption: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      languague: 'JavaScript',
+    },
+    {
+      name: 'Repo 2',
+      desciption: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      languague: 'JavaScript',
+    },
+    {
+      name: 'Repo 3',
+      desciption: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      languague: 'PHP',
+    },
+    {
+      name: 'Repo 4',
+      desciption: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      languague: null,
+    },
+    {
+      name: 'Repo 5',
+      desciption: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      languague: 'TypeScript',
+    },
+    {
+      name: 'Repo 6',
+      desciption: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      languague: 'TypeScript',
+    },
+  ];
+
+  const languages = getLangsFrom(repositories);
+
   return (
     <Container>
       <Sidebar>
-        <Profile />
-        <Filter />
+        <Profile user={user} />
+        <Filter languages={languages} />
       </Sidebar>
       <Main>
         <Repositories />
