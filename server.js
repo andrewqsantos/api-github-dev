e3057427f1411b990b679d0485686c29d03e62ee
andrewqsantos <andrewqsantos@protonmail.com>
2022-01-11 15:36:58 -0300

    heroku teste 2

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('batata');
+const express = require('express');
+
+const app = express();
+
+app.use('/', express.static(''))
+
+app.listen(process.env.PORT || 300, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Tudo funcionando certinho');
+});
