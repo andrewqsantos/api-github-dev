df4bdaad32d5e31d70401d9cfb66e5b126af7a9d
andrewqsantos <andrewqsantos@protonmail.com>
2022-01-11 15:42:33 -0300

    heroku build

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { resolve, dirname } = require('path');
 
 const app = express();
 
-app.use('/', express.static(resolve(--dirname, '.build')));
+app.use('/', express.static(resolve(__dirname, '.build')));
 
 app.listen(process.env.PORT || 300, (err) => {
   if (err) {
