41a2b2296dc47d31a2e9dc0c277e21b28f542a4e
andrewqsantos <andrewqsantos@protonmail.com>
2022-01-11 14:54:39 -0300

    mock api

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -1,78 +1,48 @@
 /* eslint-disable arrow-body-style */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useParams } from 'react-router-dom';
 
 import Profile from './Profile';
 import Filter from './Filter';
 import Repositories from './Repositories';
 
-import { Container, Sidebar, Main } from './styles';
+import { Loading, Container, Sidebar, Main } from './styles';
 
-import { getLangsFrom } from '../../services/api';
+import { getUser, getRepos, getLangsFrom } from '../../services/api';
 
 const RepositoriesPage = () => {
+  const { login } = useParams();
+
+  const [user, setUser] = useState();
+  const [repositories, setRepositories] = useState();
+  const [languages, setLanguages] = useState();
   const [currentLanguage, setCurrentLanguage] = useState();
+  const [loading, setLoading] = useState(true);
 
-  const user = {
-    login: 'andrewqsantos',
-    name: 'Andrew Quenehen',
-    avatar_url: 'https://avatars.githubusercontent.com/u/56467992?v=4',
-    followers: 36,
-    following: 59,
-    company: null,
-    blog: null,
-    location: 'Brazil',
-  };
+  useEffect(() => {
+    const loadData = async () => {
+      const [userResponse, repositoriesResponse] = await Promise.all([
+        getUser(login),
+        getRepos(login),
+      ]);
+      setUser(userResponse.data);
+      setRepositories(repositoriesResponse.data);
+      setLanguages(getLangsFrom(repositoriesResponse.data));
 
-  const repositories = [
-    {
-      id: '1',
-      name: 'Repo 1',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'JavaScript',
-    },
-    {
-      id: '2',
-      name: 'Repo 2',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'JavaScript',
-    },
-    {
-      id: '3',
-      name: 'Repo 3',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'PHP',
-    },
-    {
-      id: '4',
-      name: 'Repo 4',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: null,
-    },
-    {
-      id: '5',
-      name: 'Repo 5',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'TypeScript',
-    },
-    {
-      id: '6',
-      name: 'Repo 6',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'Ruby',
-    },
-  ];
+      setLoading(false);
+    };
 
-  const languages = getLangsFrom(repositories);
+    loadData();
+  }, []);
 
   const onFilterClick = (language) => {
     setCurrentLanguage(language);
   };
+
+  if (loading) {
+    return <Loading>Carregando...</Loading>;
+  }
 
   return (
     <Container>
